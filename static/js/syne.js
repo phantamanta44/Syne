b4649ff65ce3821b846bcc7a7d2519f79fbf4a89
phantamanta44 <evandalong@gmail.com>
2017-01-25 12:07:26 -0600

    Curve mode

--- static/js/syne.js
+++ static/js/syne.js
@@ -10,8 +10,8 @@ $(document).ready(function() {
     }
 
     function toFloats(d) {
-        var i, l = d.length;
-        var o = new Float32Array(l);
+        let i, l = d.length;
+        let o = new Float32Array(l);
         for (i = 0; i < l; i++)
             o[i] = (d[i] - 128) / 128.0;
         return o;
@@ -63,12 +63,29 @@ $(document).ready(function() {
     const Config = {
         bars: 80,
         color: "#FFF",
+        rainbow: true,
         circle: false,
         radius: 100,
-        useCurve: false,
-        outline: true,
+        curve: false,
+        lineWidth: 2,
         window: DSP.COSINE
     };
+
+    let q = {};
+    if (!!document.location.search) {
+        let parts = document.location.search.substring(1).split("&");
+        for (let i = 0; i < parts.length; i++) {
+            let eq = parts[i].indexOf("=");
+            q[parts[i].substring(0, eq).toLowerCase()] = parts[i].substring(eq + 1);
+        }
+    }
+    Config.bars = "bars" in q ? parseInt(q["bars"], 10) : Config.bars;
+    Config.color = "color" in q ? "#" + q["color"] : Config.color;
+    Config.rainbow = "rainbow" in q ? q["rainbow"] == "true" : Config.rainbow;
+    Config.circle = "circle" in q ? q["circle"] == "true" : Config.circle;
+    Config.radius = "radius" in q ? parseInt(q["radius"], 10) : Config.radius;
+    Config.curve = "curve" in q ? q["curve"] == "true" : Config.curve;
+    Config.lineWidth = "lineWidth" in q ? parseInt(q["lineWidth"], 10) : Config.lineWidth;
 
     const can = document.getElementById("vis");
     const vis = can.getContext("2d");
@@ -76,10 +93,11 @@ $(document).ready(function() {
     const windowFunc = new WindowFunction(Config.window, 1);
     const twoPi = Math.PI * 2;
     const circ = Config.radius * twoPi;
-    let barWidth, hueWidth, barWindowDelta, hBarWidth, deltaAngle;
+    let barWidth, hueWidth, barWindowDelta, hBarWidth, deltaAngle, points;
 
     function initVisualizer() {
-        hueWidth = 360 / Config.bars;
+        if (Config.rainbow)
+            hueWidth = 360 / Config.bars;
         if (!Config.circle) {
             barWindowDelta = 6 / Config.bars;
         } else {
@@ -88,6 +106,8 @@ $(document).ready(function() {
             barWindowDelta = 12 / Config.bars;
             deltaAngle = twoPi / Config.bars;
         }
+        if (Config.curve)
+            points = new Array(Config.bars);
         updateBars();
     }
 
@@ -97,7 +117,7 @@ $(document).ready(function() {
         aNode.getByteTimeDomainData(buf);
         buf = detrend(windowFunc.process(toFloats(buf)));
         fft.forward(buf);
-        let data = fft.spectrum.subarray(1, fft.spectrum.length / 3 + 1);
+        let data = fft.spectrum.subarray(1, Math.floor(fft.spectrum.length / 3 + 1));
         let secL = Math.floor(data.length / Config.bars);
         for (let i = 0; i < Config.bars; i++) {
             let avg = 0;
@@ -109,40 +129,82 @@ $(document).ready(function() {
             else
                 yVal[i] *= 0.94;
         }
+        console.log(yVal.length);
         can.width = window.innerWidth;
         can.height = window.innerHeight;
+        let hcw = can.width / 2;
+        let hch = can.height / 2;
         if (!Config.circle) {
             vis.clearRect(0, 0, can.width, can.height);
-            barWidth = can.width / Config.bars;
-            for (let i = 0; i < Config.bars; i++) {
-                vis.fillStyle = "hsl(" + Math.floor(hueWidth * i) + ", 100%, 32%)";
-                let height = Math.max(yVal[i] * 500 * (1 + i * barWindowDelta), 2);
-                vis.fillRect(i * barWidth + 0.5, can.height / 2 - height / 2 + 0.5, barWidth, height);
+            barWidth = can.width / (Config.bars - 2);
+            if (Config.curve) {
+                vis.beginPath();
+                vis.moveTo(0.5, hch - Math.max(yVal[0] * 500, 2) + 0.5);
+                vis.lineWidth = Config.lineWidth;
+                let i;
+                for (let i = 0; i < Config.bars; i++) {
+                    let height = Math.max(yVal[i] * 500 * (1 + i * barWindowDelta), 2);
+                    points[i] = {
+                        x: i * barWidth + 0.5,
+                        y: hch - height + 0.5
+                    };
+                }
+                for (i = 1; i < points.length - 2; i++) {
+                    let xC = (points[i].x + points[i + 1].x) / 2;
+                    let yC = (points[i].y + points[i + 1].y) / 2;
+                    vis.quadraticCurveTo(points[i].x, points[i].y, xC, yC);
+                }
+                vis.quadraticCurveTo(points[i].x, points[i].y, points[i + 1].x, points[i + 1].y);
+                vis.strokeStyle = Config.color;
+                vis.stroke();
+            } else {
+                vis.fillStyle = Config.color;
+                for (let i = 0; i < Config.bars; i++) {
+                    if (Config.rainbow)
+                        vis.fillStyle = "hsl(" + Math.floor(hueWidth * i) + ", 100%, 32%)";
+                    let height = Math.max(yVal[i] * 500 * (1 + i * barWindowDelta), 2);
+                    vis.fillRect(i * barWidth + 0.5, hch - height / 2 + 0.5, barWidth, height);
+                }
             }
         } else {
-            vis.clearRect(-can.width / 2, -can.height / 2, can.width, can.height);
-            vis.translate(can.width / 2, can.height / 2);
-            for (let i = 0; i < Config.bars; i++) {
-                vis.fillStyle = "hsl(" + Math.floor(hueWidth * i) + ", 100%, 32%)";
-                let height = Math.max(yVal[i] * 150 * (1 + i * barWindowDelta), 2);
-                vis.fillRect(-hBarWidth, -Config.radius - height, barWidth, height);
-                vis.rotate(deltaAngle);
+            vis.clearRect(-hcw, -hch, can.width, can.height);
+            vis.translate(hcw, hch);
+            if (Config.curve) {
+                let i;
+                for (i = 0; i < Config.bars; i++) {
+                    let height = Math.max(yVal[i] * 150 * (1 + i * barWindowDelta), 2);
+                    let newRad = Config.radius + height;
+                    points[i] = {
+                        x: newRad * Math.sin(deltaAngle * i),
+                        y: newRad * -Math.cos(deltaAngle * i)
+                    };
+                }
+                vis.beginPath();
+                vis.moveTo(points[0].x, points[0].y);
+                vis.lineWidth = Config.lineWidth;
+                for (i = 1; i < points.length - 1; i++) {
+                    let xC = (points[i].x + points[i + 1].x) / 2;
+                    let yC = (points[i].y + points[i + 1].y) / 2;
+                    vis.quadraticCurveTo(points[i].x, points[i].y, xC, yC);
+                }
+                vis.quadraticCurveTo(points[i].x, points[i].y, points[0].x, points[0].y);
+                vis.strokeStyle = Config.color;
+                vis.stroke();
+            } else {
+                vis.fillStyle = Config.color;
+                for (let i = 0; i < Config.bars; i++) {
+                    if (Config.rainbow)
+                        vis.fillStyle = "hsl(" + Math.floor(hueWidth * i) + ", 100%, 32%)";
+                    let height = Math.max(yVal[i] * 150 * (1 + i * barWindowDelta), 2);
+                    vis.fillRect(-hBarWidth + 0.5, -Config.radius - height + 0.5, barWidth, height);
+                    vis.rotate(deltaAngle);
+                }
             }
         }
         window.setTimeout(updateBars, 0);
     }
 
     prepareAudioNodes();
-
-    let q = {};
-    if (!!document.location.search) {
-        let parts = document.location.search.substring(1).split("&");
-        for (let i = 0; i < parts.length; i++) {
-            let eq = parts[i].indexOf("=");
-            q[parts[i].substring(0, eq).toLowerCase()] = parts[i].substring(eq + 1);
-        }
-    }
-    Config.circle = "circle" in q ? q["circle"] == "true" : false;
     loadSound(q.url || "test.mp3");
 
 });
